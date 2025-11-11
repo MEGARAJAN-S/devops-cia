60e4db7d907166b4272ffbf196986a3a7be312b2
MEGARAJAN <63449890+MEGARAJAN-S@users.noreply.github.com>
2025-11-11 21:01:51 +0530

    Add 'Change Check 01' heading to App component

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
       <h1>Hi, CI/CD!</h1>
       <p>My React app is running from Jenkins!</p>
       <h1>Megarajan S IoT A 22011102054</h1>
+      <h2>Change Check 01</h2>
     </div>
   )
 }
